a26d724d0fb12a7cf99c723a6bf40b70a6cf1d55
M J Chowdhury <bdjunayed@gmail.com>
2017-03-22 11:58:56 +0600

    test mail start

--- sendmail.php
+++ sendmail.php
@@ -1,20 +1,50 @@
 <?php
-   
+$con_name = $_POST['name'];
+$con_email = $_POST['email'];
+$con_company = $_POST['companyName'];
+$con_subject = $_POST['subject'];
+$con_message = $_POST['message'];
+
+$to ='bdjunayed@gmail.com';
+
+$message = 'Name: ' .$con_name.'<br/><br/>';
+$message .= 'Email:' .$con_email.'<br/><br/>';
+$message .= 'Company:' .$con_company.'<br/><br/>';
+$message .= 'Message' .$con_message.'<br/><br/>';
+$message .= 'Best Regards, from www.microtchna.com';
+
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$headers .= 'From: <'.$con_email.'>' . "\r\n";
+$headers .= 'Reply-To: <'.$con_email.'>' . "\r\n";
+//$headers .= 'Cc: bdjunayed@hotmail.com' . "\r\n";
+//$headers .= 'Bcc: bdjunayed@yahoo.com' . "\r\n";
+
+$mail_sent = @mail($to, $con_subject, $message, $headers);
+
+
+echo $mail_sent ? include "success.php" : include "failed.php" ;
+
+header('location: conatct.php');
+?>
+
+<?php
+/*
     if(isset($_POST['submit'])){
         $message= 'name: '.$_POST['Name'] ."\n"
                 .'Email' .$_POST['Email'] ."\n"
                 . 'Company Name: ' .$_POST['CompanyName'];
-        $sub= 'Message: ' .$_POST['Message'];
         $subject='Subject' .$_POST['subject'];
-                
+        $sub= 'Message: ' .$_POST['Message'];
+
         $to= 'abrasel600@gmail.com';
         mail ($to,$message, $sub,$subject);
-        //header('location: send_successfully_done.php');
+
         echo "your message sent <a href=''>click here</a> another mail";
     }
  else {
         header('location: workwith.html');
         exit(0);
 }
-    
-?>
+
+*/?>
