203fb12964e3749dfe468d6d9d10bb36eeb1bba5
rasel <abrasel600@gmail.com>
2017-03-23 11:10:58 +0600

    ok

--- sendmail.php
+++ sendmail.php
@@ -1,50 +1,32 @@
 <?php
-$con_name = $_POST['name'];
-$con_email = $_POST['email'];
-$con_company = $_POST['companyName'];
-$con_subject = $_POST['subject'];
-$con_message = $_POST['message'];
+//old scripts
+//print_r($_POST);
+$to = 'APMS<apmsglobal@gmail.com>';
 
-$to ='bdjunayed@gmail.com';
+$subject = 'Feedback from MicorTechNA: ' . $_POST["name"];
 
-$message = 'Name: ' .$con_name.'<br/><br/>';
-$message .= 'Email:' .$con_email.'<br/><br/>';
-$message .= 'Company:' .$con_company.'<br/><br/>';
-$message .= 'Message' .$con_message.'<br/><br/>';
-$message .= 'Best Regards, from www.microtchna.com';
+$message = "Feedback from MicroTech:\n\n" 
+."\n\nName: ". $_POST["name"]
+."\n\nCompany Name: ". $_POST["companyName"]
+."\n\nEmail: ". $_POST["email"]
+."\n\nSubject: ". $_POST["subject"]
+."\n\nMessage: ". $_POST["message"]
+."\n\n\nthis is a feedback from micortechna.com";
+
 
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-$headers .= 'From: <'.$con_email.'>' . "\r\n";
-$headers .= 'Reply-To: <'.$con_email.'>' . "\r\n";
-//$headers .= 'Cc: bdjunayed@hotmail.com' . "\r\n";
-//$headers .= 'Bcc: bdjunayed@yahoo.com' . "\r\n";
-
-$mail_sent = @mail($to, $con_subject, $message, $headers);
-
-
-echo $mail_sent ? include "success.php" : include "failed.php" ;
-
-header('location: conatct.php');
-?>
-
-<?php
-/*
-    if(isset($_POST['submit'])){
-        $message= 'name: '.$_POST['Name'] ."\n"
-                .'Email' .$_POST['Email'] ."\n"
-                . 'Company Name: ' .$_POST['CompanyName'];
-        $subject='Subject' .$_POST['subject'];
-        $sub= 'Message: ' .$_POST['Message'];
-
-        $to= 'abrasel600@gmail.com';
-        mail ($to,$message, $sub,$subject);
-
-        echo "your message sent <a href=''>click here</a> another mail";
-    }
- else {
-        header('location: workwith.html');
-        exit(0);
+$headers .= 'From: <'.$_POST["name"] . $_POST["email"].'>'."\r\n";
+$headers .= 'Reply-To: <'.$_POST["email"].'>' . "\r\n";
+if(mail( $to, $subject, $message, $headers ))
+{
+    header('location: index.html');
 }
+else{
+    echo 'error';
+}
+//echo $mail_sent ? include "index.html" : include "work.html" ;
+exit(0);
 
-*/?>
+
+?>
